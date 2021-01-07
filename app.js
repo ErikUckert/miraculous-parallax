e0170b35e369276e8855f035f678cb1cfd9cb3b1
Erik Uckert <uckert@outlook.com>
2021-01-07 10:29:32 +0100

    calculate distance between content & img container to prevent overlapping

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const header = document.querySelector("header");
 const shadow = document.querySelector(".shadow");
 const content = document.querySelector(".content");
 const section = document.querySelector("section");
-const image_container = document.querySelector(".imgContainer");
+const imageContainer = document.querySelector(".imgContainer");
 const opacity = document.querySelectorAll(".opacity");
 const border = document.querySelector(".border");
 const container = document.querySelector(".container");
@@ -21,7 +21,7 @@ window.addEventListener('scroll', () => {
     // get vertical offset when scrolling
     let scroll = window.scrollY;
 
-    // get spatial informations about the scrolled section (size & position)
+    // get bounding rectangles for spatial informations about the scrolled section (size & position)
     let sectionY = section.getBoundingClientRect();
 
     // prevent content & header colision by changing header visibility based on scroll position
@@ -57,9 +57,29 @@ window.addEventListener('scroll', () => {
     // shadow animation under parallax image
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
+    
+    // get content bounding rectangle
+    let contentRect = content.getBoundingClientRect();
+    
+    // get content center point
+    let contentRectX = contentRect.left + contentRect.width/2;
+    var contentRectY = contentRect.top + contentRect.height/2;
+
+    // get image container bounding rectangle
+    let imageContainerRect = imageContainer.getBoundingClientRect();
+
+    // get image container center point
+    var imageContainerRectX = imageContainerRect.left + imageContainerRect.width/2;
+    var imageContainerRectY = imageContainerRect.top + imageContainerRect.height/2;
+
+    // calculate the distance using the Pythagorean Theorem (a^2 + b^2 = c^2)
+    var distanceSquared = Math.pow(contentRectX - imageContainerRectX, 2) + Math.pow(contentRectY  - imageContainerRectY, 2);
+    var distance = Math.sqrt(distanceSquared);
+    console.log(distance);
+
     // move content & image element vertical to align in mid page
     content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
-    image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -20 + 50}px)`;
+    imageContainer.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -20 + 50}px)`;
 
     // let hyphen grow, baby!
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
